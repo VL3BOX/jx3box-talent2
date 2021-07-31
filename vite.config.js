0b9baac576219df222887fd1d2ea3a0a916ce669
Kaviilee <jiaweilee95@126.com>
2021-08-01 00:33:42 +0800

    build: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
-  plugins: [createVuePlugin()]
+  plugins: [createVuePlugin()],
+  base: './'
 })
