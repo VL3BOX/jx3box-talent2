234537683be0bc279964efc594e8277b6f2b3373
X3ZvaWQ <x3zvawq@gmail.com>
2023-09-12 13:08:04 +0800

    fix: 构建错误

--- tools/build.js
+++ tools/build.js
@@ -1,42 +1,42 @@
 /*
-* Desc: 构建镇派数据
-* Author : iRuxu
-* Email : rx6@qq.com
-* Time :
-*/
+ * Desc: 构建镇派数据
+ * Author : iRuxu
+ * Email : rx6@qq.com
+ * Time :
+ */
 const fs = require("fs");
 const path = require("path");
 const dateFormat = require("./includes/dateFormat");
 const filter = require("./includes/filter.js");
-const { Logger, readTabFile, buildProgressBar } = require('./includes/utils');
+const { Logger, readTabFile, buildProgressBar } = require("./includes/utils");
 
 const dist = "../output";
 
 (async () => {
     Logger.info("========== 开始构建镇派数据 ==========");
-    Logger.info('读取必要的raw数据......');
+    Logger.info("读取必要的raw数据......");
     const datas = {
         raw: require("../raw/talent.json"),
-        xf: require('@jx3box/jx3box-data/data/xf/xf.json'),
+        xf: require("@jx3box/jx3box-data/data/xf/xf.json"),
         talent_relation: require("../raw/talent_ralation.json"),
         talent_pretab: require("../raw/talent_pretab.json"),
         skill: [],
-        skill_txt: (await readTabFile(path.join(__dirname, '../raw/skill.txt'))).reduce((acc, cur) => {
+        skill_txt: (await readTabFile(path.join(__dirname, "../raw/skill.txt"))).reduce((acc, cur) => {
             let key = `${cur.SkillID}_${cur.Level}`;
             acc[key] = cur;
             return acc;
         }, {}),
-        skill_tab: (await readTabFile(path.join(__dirname, '../raw/skills.tab'))).reduce((acc, cur) => {
+        skill_tab: (await readTabFile(path.join(__dirname, "../raw/skills.tab"))).reduce((acc, cur) => {
             acc[cur.SkillID] = cur;
             return acc;
         }, {}),
         buff: [],
-        buff_txt: (await readTabFile(path.join(__dirname, '../raw/buff.txt'))).reduce((acc, cur) => {
+        buff_txt: (await readTabFile(path.join(__dirname, "../raw/buff.txt"))).reduce((acc, cur) => {
             let key = `${cur.BuffID}_${cur.Level}`;
             acc[key] = cur;
             return acc;
         }, {}),
-        buff_tab: (await readTabFile(path.join(__dirname, '../raw/buff.tab'))).reduce((acc, cur) => {
+        buff_tab: (await readTabFile(path.join(__dirname, "../raw/buff.tab"))).reduce((acc, cur) => {
             let key = `${cur.ID}_${cur.Level}`;
             cur.BuffID = cur.ID;
             cur.BuffName = cur.Name;
@@ -46,11 +46,11 @@ const dist = "../output";
             return acc;
         }, {}),
         output: {},
-        talent2: {}
+        talent2: {},
     };
-    Logger.info('合并需要用到的 skill 数据......');
+    Logger.info("合并需要用到的 skill 数据......");
     {
-        let bar = await buildProgressBar(Object.keys(datas.skill_txt).length, '合并需要用到的 skill 数据');
+        let bar = await buildProgressBar(Object.keys(datas.skill_txt).length, "合并需要用到的 skill 数据");
         for (let k in datas.skill_txt) {
             let skill = datas.skill_txt[k];
             let _skill = datas.skill_tab[skill.SkillID];
@@ -60,7 +60,7 @@ const dist = "../output";
             datas.skill.push(skill);
             bar.tick();
         }
-        let remain_skills = Object.values(datas.skill_tab).filter(t => !t.__scan);
+        let remain_skills = Object.values(datas.skill_tab).filter((t) => !t.__scan);
         bar.total = bar.total + remain_skills.length;
         for (let skill of remain_skills) {
             datas.skill.push(skill);
@@ -68,9 +68,9 @@ const dist = "../output";
         }
         bar.terminate();
     }
-    Logger.info('合并需要用到的 buff 数据.....');
+    Logger.info("合并需要用到的 buff 数据.....");
     {
-        bar = await buildProgressBar(Object.keys(datas.buff_tab).length, 'buff合表中...', '...');
+        bar = await buildProgressBar(Object.keys(datas.buff_tab).length, "buff合表中...", "...");
         for (let buffid in datas.buff_tab) {
             let buff = datas.buff_tab[buffid];
             let _buff = datas.buff_txt[`${buff.BuffID}_${buff.Level}`];
@@ -87,15 +87,16 @@ const dist = "../output";
             bar.tick();
         }
         bar.terminate();
-        Logger.info('构建镇派数据......');
+        Logger.info("构建镇派数据......");
     }
     //需要用到的方法
-    const getSkill = async (id) => {
+    const getSkill = (id) => {
         if (!id) return null;
         let res = datas.skill.filter((t) => t.SkillID == id);
         const length = res.length;
-        let max = ~~(res[length - 1]['MaxLevel']);
-        const _item = res.find(l => l.Level == 1); // 图标获取技能等级为1的数据
+        if (!length) return null;
+        let max = ~~res[length - 1]["MaxLevel"];
+        const _item = res.find((l) => l.Level == 1); // 图标获取技能等级为1的数据
         let item = _item || res[max - 1];
         // let max = Math.max(length - 1, 1);    //层数
         let desc;
@@ -103,14 +104,14 @@ const dist = "../output";
         if (length > 1) {
             const first = res[0];
             const skillList = parseInt(first.Level) ? res : res.slice(1, length);
-            desc = skillList.map(d => d['Desc']);
-            desc = await Promise.all(desc.map(d1 => filter(d1, datas)));
-            desc.map(d2 => d2 && d2.replace(/"/g, "'"))
+            desc = skillList.map((d) => d["Desc"]);
+            desc = desc.map((d1) => filter(d1, datas));
+            desc.map((d2) => d2 && d2.replace(/"/g, "'"));
         } else {
             desc = res[res.length - 1]["Desc"];
-            desc = await filter(desc, datas); //过滤后的奇穴描述
+            desc = filter(desc, datas); //过滤后的奇穴描述
             desc = desc.replace(/"/g, "'"); //奇穴内容的"规范为单引号
-            desc = [desc]
+            desc = [desc];
         }
         return {
             id: id,
@@ -119,36 +120,42 @@ const dist = "../output";
             desc: desc,
             // type: item.SpecialDesc ? "skill" : "talent",
             type: !!~~item.IsPassiveSkill ? "talent" : "skill",
-            children: [],   //已废弃
-            pretab: datas.talent_pretab[id] || '',
+            children: [], //已废弃
+            pretab: datas.talent_pretab[id] || "",
             max: max,
         };
-    }
-    bar = await buildProgressBar(Object.keys(datas.raw).length * 6 * 4, '构建镇派数据');
-    for (let k in datas.raw) {
+    };
+    bar = await buildProgressBar(Object.keys(datas.raw).length * 6 * 4, "构建镇派数据");
+    for (let xf_name in datas.raw) {
         // 固定6行，每行4个
-        datas.output[datas.talent_relation[k]] = [[], [], [], [], [], []];
-        let list = datas.raw[k];
+        datas.output[datas.talent_relation[xf_name]] = [];
+        const xf_talent = datas.output[datas.talent_relation[xf_name]];
+        const xf_raw = datas.raw[xf_name];
         // 每一行
-        for (let r = 0; r < list.length; r++) {
-            let row = list[r];
-            for (let i = 0; i < row.length; i++) {
-                let item = row[i];
-                let res = await getSkill(item);
-                datas.output[datas.talent_relation[k]][r][i] = res;
-                bar.tick(1, { doing: `${k} | ${res?.name}(${res?.id})`, title: bar.title });
+        for (let row in xf_raw) {
+            xf_talent.push([]);
+            const row_data = xf_raw[row];
+            for (let col in row_data) {
+                const skill_id = row_data[col];
+                const skill = getSkill(skill_id);
+                if(!skill && skill_id) {
+                    bar.clear();
+                    Logger.warn(`心法 ${xf_name} 的第 ${row + 1} 行第 ${col + 1} 列的镇派技能 ID ${skill_id} 不存在`);
+                }
+                xf_talent[row][col] = skill;
+                bar.tick();
             }
         }
     }
     bar.terminate();
-    Logger.success('镇派构建结束，进行 renderBpsTalent2 ......');
+    Logger.success("镇派构建结束，进行 renderBpsTalent2 ......");
     for (let talent in datas.raw) {
-        let mountID = datas.xf[talent]['id'];
-        datas.talent2[mountID] = datas.raw[talent].map(item => item.filter(t => t));
+        let mountID = datas.xf[talent]["id"];
+        datas.talent2[mountID] = datas.raw[talent].map((item) => item.filter((t) => t));
     }
-    Logger.success('renderBpsTalent2 结束，输出结果');
+    Logger.success("renderBpsTalent2 结束，输出结果");
     fs.writeFileSync(path.join(__dirname, dist, `/v${dateFormat(new Date())}.json`), JSON.stringify(datas.output));
     fs.writeFileSync(path.join(__dirname, dist, `/talent2.json`), JSON.stringify(datas.talent2));
     fs.writeFileSync(path.join(__dirname, `../src/data/talent2.json`), JSON.stringify(datas.talent2));
-    Logger.success('结果已输出，脚本运行结束');
+    Logger.success("结果已输出，脚本运行结束");
 })();
--- tools/includes/filter.js
+++ tools/includes/filter.js
@@ -1,139 +1,138 @@
 //对数据库查询的desc字段进行数据加工
-const _ = require('lodash');
-const axios = require('axios');
+const _ = require("lodash");
+const axios = require("axios");
 
-async function Filter(desc, datas) {
+function Filter(desc, datas) {
     if (!desc) return desc;
     //处理sub
-    desc = filterBySub(desc, datas)
+    desc = filterBySub(desc, datas);
 
     //处理buff描述
-    desc = filterByBuffDesc(desc, datas)
+    desc = filterByBuffDesc(desc, datas);
 
     //处理buff时间
-    desc = filterByBuffTime(desc, datas)
+    desc = filterByBuffTime(desc, datas);
 
     //处理其他类型的buff -- 基于skill@22635改进,2019/11/8
-    desc = filterByBuffAt(desc)
+    desc = filterByBuffAt(desc);
 
     //处理额外附加攻击   -- 基于skill@22635改进,2019/11/8
-    desc = filterByAdd(desc)
+    desc = filterByAdd(desc);
 
     //过滤其它<>
-    desc = desc.replace(/\<.*?\>/g, '')
+    desc = desc.replace(/\<.*?\>/g, "");
 
     //处理undefined
-    desc = filterUndefined(desc)
+    desc = filterUndefined(desc);
 
     //替换回车符
-    desc = desc.replace(/\\n/g, '<br/>')
+    desc = desc.replace(/\\n/g, "<br/>");
 
-    return desc
+    return desc;
 }
 
 function get_resource(type, query, datas) {
     let id_map = {
-        skill: 'SkillID',
-        buff: 'BuffID'
+        skill: "SkillID",
+        buff: "BuffID",
     };
     let primaryKey = id_map[type];
     if (!primaryKey) return [];
-    return datas[type].filter(item => item[primaryKey] == query);
+    return datas[type].filter((item) => item[primaryKey] == query);
 }
 
 function filterBySub(desc, datas) {
-    let reg = new RegExp(/\<SUB (\d+?) (\d)\>/g)
-    let subreg = new RegExp(/\<SUB (\d+?) (\d)\>/)
-    let hasMatched = reg.test(desc)
+    let reg = new RegExp(/\<SUB (\d+?) (\d)\>/g);
+    let subreg = new RegExp(/\<SUB (\d+?) (\d)\>/);
+    let hasMatched = reg.test(desc);
 
     if (hasMatched) {
-        let arr = desc.match(reg)
+        let arr = desc.match(reg);
         for (let i = 0; i < arr.length; i++) {
-            let capture = subreg.exec(arr[i])
-            let id = capture[1]
-            let level = capture[2]
+            let capture = subreg.exec(arr[i]);
+            let id = capture[1];
+            let level = capture[2];
 
-            let result = get_resource('skill', id, datas)
-            let skill = {}
+            let result = get_resource("skill", id, datas);
+            let skill = {};
             _.each(result, function (obj, i) {
                 if (obj.Level == level) {
-                    skill = obj
-                    return
+                    skill = obj;
+                    return;
                 }
-            })
+            });
 
             if (skill.Desc) {
-                desc = desc.replace(arr[i], skill.Desc)
+                desc = desc.replace(arr[i], skill.Desc);
             }
-
         }
     }
-    return desc
+    return desc;
 }
 function filterByBuffDesc(desc, datas) {
-    let reg = new RegExp(/\<BUFF (\d+?) \d?\ desc>/)
-    let hasMatched = reg.test(desc)
+    let reg = new RegExp(/\<BUFF (\d+?) \d?\ desc>/);
+    let hasMatched = reg.test(desc);
     if (hasMatched) {
-        let capture = reg.exec(desc)
-        let id = capture[1]
+        let capture = reg.exec(desc);
+        let id = capture[1];
         //let level = capture[2]
 
-        let result = get_resource('buff', id, datas)
-        let buff = result.find(item => item.Desc);
+        let result = get_resource("buff", id, datas);
+        let buff = result.find((item) => item.Desc);
 
         if (buff) {
-            desc = desc.replace(reg, buff.Desc)
+            desc = desc.replace(reg, buff.Desc);
         }
     }
-    return desc
+    return desc;
 }
 function filterByBuffTime(desc, datas) {
-    let reg = new RegExp(/\<BUFF (\d+?) (\d?)\ time>/)
-    let buffTimeReg = new RegExp(/\<BUFF (\d+?) (\d?)\ time>/g)
-    let hasMatched = reg.test(desc)
+    let reg = new RegExp(/\<BUFF (\d+?) (\d?)\ time>/);
+    let buffTimeReg = new RegExp(/\<BUFF (\d+?) (\d?)\ time>/g);
+    let hasMatched = reg.test(desc);
     if (hasMatched) {
-        let capture = reg.exec(desc)
-        let id = capture[1]
+        let capture = reg.exec(desc);
+        let id = capture[1];
 
-        let result = get_resource('buff', id, datas)
+        let result = get_resource("buff", id, datas);
 
         // buff 持续时间 示例 持续1秒-5秒
-        const _capture = desc.match(buffTimeReg)
+        const _capture = desc.match(buffTimeReg);
 
-        _capture.forEach(cap => {
-            const tmp = reg.exec(cap)
+        _capture.forEach((cap) => {
+            const tmp = reg.exec(cap);
 
-            let buff = result.find(item => item.Level === Number(tmp[2])) || result[0]
-            if (!buff.Interval) buff = result[1]
-            let time = parseInt(buff.Interval) / 16 * parseInt(buff.Count) + '秒'
-            desc = desc.replace(reg, time)
-        })
+            let buff = result.find((item) => item.Level === Number(tmp[2])) || result[0];
+            if (!buff.Interval) buff = result[1];
+            let time = (parseInt(buff.Interval) / 16) * parseInt(buff.Count) + "秒";
+            desc = desc.replace(reg, time);
+        });
     }
-    return desc
+    return desc;
 }
 //(+<SKILLEx {D0} {TotalPhysicsAP 0.3906}>)
 function filterByAdd(desc) {
-    let reg = new RegExp(/\(\+\<SKILLEx.*?\>\)/g)
-    let hasMatched = reg.test(desc)
+    let reg = new RegExp(/\(\+\<SKILLEx.*?\>\)/g);
+    let hasMatched = reg.test(desc);
 
     if (hasMatched) {
-        desc = desc.replace(reg, '')
+        desc = desc.replace(reg, "");
     }
-    return desc
+    return desc;
 }
 //<BUFF atCallPhysicsDamage>
 function filterByBuffAt(desc) {
-    let reg = new RegExp(/<BUFF at.*?>/g)
-    let hasMatched = reg.test(desc)
+    let reg = new RegExp(/<BUFF at.*?>/g);
+    let hasMatched = reg.test(desc);
 
     if (hasMatched) {
-        desc = desc.replace(reg, '')
+        desc = desc.replace(reg, "");
     }
-    return desc
+    return desc;
 }
 
 function filterUndefined(desc) {
-    return desc.replace(/undefined/g, '*')
+    return desc.replace(/undefined/g, "*");
 }
 
-module.exports = Filter
+module.exports = Filter;
